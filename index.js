62851a563820af34aad2513101b44f5d174fb279
ashutosh-dgksocial <ashutoshdgksocial@gmail.com>
2024-12-28 02:58:34 +0530

    learning

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const express = require('express')
 const app = express()
+const dotenv = require("dotenv");
+
+dotenv.config();
 const port = 3000
 
+
 app.use(express.urlencoded({ extended: true }));// for parsing application/x-www-form-urlencoded
+const mongoose = require("mongoose");
+//connection to db
+mongoose.set("useFindAndModify", false);
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
+console.log("Connected to db!");
+app.listen(3000, () => console.log("Server Up and running"));
+});
+
 app.set("view engine", "ejs"); // for rendering ejs files
+
+app.use("/static", express.static("public")); // for serving static files
+
 
 app.get('/', (req, res) => {
     res.render("todo.ejs");  // for rendering the form
 })
+
+app.post('/',(req, res) => {
+    console.log(req.body);
+    });
+    
 
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
